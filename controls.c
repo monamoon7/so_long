7da763774b918087860299a68a1519eec545834c
Mona Shaghafi <mona.shaghafi@gmail.com>
2025-05-16 14:44:11 +0200

    changes

--- controls.c
+++ controls.c
@@ -6,26 +6,47 @@
 /*   By: mona <mona@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/05/16 12:21:38 by mona          #+#    #+#                 */
-/*   Updated: 2025/05/16 12:34:03 by mona          ########   odam.nl         */
+/*   Updated: 2025/05/16 14:44:01 by mona          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+static void	print_move_count(int count)
+{
+	ft_putstr("Move: ");
+	ft_putnbr(count);
+	ft_putstr("\n");
+}
+
 static void	move_player(t_game *game, int dx, int dy)
 {
-	int	new_x = game->player_x + dx;
-	int new_y = game->player_y + dy;
-	char	next_tile = game->map[new_y][new_x];
-	
-	if (next_tile = '1')
-		return;
-	if (next_tile = 'E' && has_collectibles(game))
-		return;
-	if (next_tile == 'C')
+	int		new_x;
+	int		new_y;
+	char	tile;
+
+	new_x = game->player_x + dx;
+	new_y = game->player_y + dy;
+	tile = game->map[new_y][new_x];
+	if (tile == '1')
+		return ;
+	if (tile == 'E' && has_collectibles(game))
+		return ;
+	if (tile == 'C')
 		game->map[new_y][new_x] = '0';
-	if (next_tile == 'E')
+	if (tile == 'E')
 	{
-		
+		game->move_count++;
+		ft_putstr("You win in ");
+		ft_putnbr(game->move_count);
+		ft_putstr(" moves!\n");
+		exit_game(game);
 	}
+	game->map[game->player_y][game->player_x] = '0';
+	game->map[new_y][new_x] = 'P';
+	game->player_x = new_x;
+	game->player_y = new_y;
+	game->move_count++;
+	print_move_count(game->move_count);
+	render_map(game);
 }
